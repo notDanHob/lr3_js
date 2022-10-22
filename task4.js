9eebe06de86f9af817815cd8cc5468ce91b476d2
Sukhomlinov Nikita <nsm200216@gmail.com>
2022-10-22 13:23:52 +0300

    added comment

--- task4.js
+++ task4.js
@@ -2,7 +2,7 @@ function polindrom(string) {
   rev_string = string.split("").reverse().join("");
   return string.toLowerCase() == rev_string.toLowerCase();
 }
-
+//функция не берет в учет регистр букв, так как по факту это все равно одна и та же буква
 console.log(polindrom("An na")); //true
 console.log(polindrom("Johnny gooks they are in the trees")); //false
 console.log(polindrom("asdf,.,fdsa")); //true
